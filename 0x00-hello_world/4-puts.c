30f981e1f72883d1b5511dde8f7652f6d389e21a
mccallum8219 <mccallum8219@gmail.com>
2023-01-28 08:06:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
+include <stdio.h>;
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* main - main block
+* Return: o
 */
 int main(void)
 {
     puts ("\"Programming is like building a multilingual puzzle");
-    return (0);
-    }
+    return (*);
+}

e0b18680ef34d16fa8eccc1dddb1ba5ff33eab49
mccallum8219 <mccallum8219@gmail.com>
2023-01-30 20:17:47 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
- */
-
+ * main - Prints putchar
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints putchar
+ *
+ * Return: 0 for success
+*/
 int main(void)
 {
-  
-  	_putchar('_'); 
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -18,5 +19,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 }

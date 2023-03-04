ef17f53045aaf58b06fa588629baac5c0789ba4a
mccallum8219 <mccallum8219@gmail.com>
2023-03-04 13:37:01 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,14 +33,17 @@ char*_strcpy(char*dest, char *src)
 	int len, i;
 	len = 0;
 	while (src[len] !='\0')
+
 	{
 		len++;
 	}
 
 	for (i=0; i<len; i++)
+
 	{
 		dest[i] = src[i];
 	}
+
 	dest[i] ='\0';
 	return (dest);
 }
@@ -59,24 +62,28 @@ dog_t *new_dog(char*name, float age, char*owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
+
 	{
 		free(dog);
 		return (NULL);
 	}
+
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
+
 	{
 		free(dog);
 		free(dog->name);
 		return (NULL);
 	}
+
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;

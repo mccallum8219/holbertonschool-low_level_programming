8d6160e8459e51932c18364118f68b92b60b5d07
mccallum8219 <mccallum8219@gmail.com>
2023-03-04 16:31:15 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MARCO_H
-#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+#define ABS(X) ((X) < 0 ? -(X) : (X))
+
 #endif

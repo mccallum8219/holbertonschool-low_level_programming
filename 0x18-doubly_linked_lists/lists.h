919b90a5138e64ac97fa3f4fd570284061af1003
root <mccallum8219@gmail.com>
2023-03-27 15:24:01 -0700

    0-print_dlistint.c

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -23,7 +23,7 @@ typedef struct dlistint_s;
     struct dlistint_s *next;
 } dlistint_t;
 
-Prototype: size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
@@ -32,4 +32,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 size_t print_dlistint_backward(const dlistint_t *h);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-endif
+#endif
